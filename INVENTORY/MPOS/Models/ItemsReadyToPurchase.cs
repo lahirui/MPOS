7a0ce238ea76de96ebd223ffae2b7145c236f638
lahirui <lahirui@sl.crystal-martin.com>
2020-10-29 17:38:39 +0530

    Re-Added MPOS Inventory

---
+++ INVENTORY/MPOS/Models/ItemsReadyToPurchase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MPOS.Models
+{
+    public class ItemsReadyToPurchase
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public int EmployeeID { get; set; }
+        public int UserId { get; set; }
+        public Nullable<decimal> ItemQty { get; set; }
+        public Nullable<decimal> UnitPrice { get; set; }
+        public Nullable<decimal> ItemTotal { get; set; }
+    }
+}
